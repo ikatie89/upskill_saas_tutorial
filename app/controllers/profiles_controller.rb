acd671d56c43ac58a80307ac44986d41e0a4c6da
Katarzyna Sieczkowska <courageous89@o2.pl>
2017-09-22 10:39:55 +0000

    Added user profiles form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,6 +3,7 @@ class ProfilesController < ApplicationController
   # GET to users/:user_id/profile/new
   def new
     # render blank profile details form
+    @profile = Profile.new
   end
   
 end
